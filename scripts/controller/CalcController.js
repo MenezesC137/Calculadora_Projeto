383e0a2b05e88edbd76f81e2fd17f6017ede94bd
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-02-25 10:47:16 -0300

    Encapsulamento

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,27 @@
 class CalcController{
 
+    constructor(){
 
-    
+        this._displaycalc = '0';
+        this._dataAtual;
+    }
+
+    get displaycalc(){
+
+        return this._displaycalc;
+    }
+
+    set displaycalc(valor){
+        this._displaycalc = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(data){
+
+        this._dataAtual = data;
+    }
+
 }
