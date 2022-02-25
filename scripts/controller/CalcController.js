3e123884bf225e6524d737b08056c1a205eea14d
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-02-25 11:25:38 -0300

    Date and Time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,8 +4,20 @@ class CalcController{
 
         this._displaycalc = '0';
         this._currentDate;
+        this.initialize();
     }
 
+    initialize(){
+
+        let displaycalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+
+        displaycalcEl.innerHTML ="1325";
+        dateEl.innerHTML ="25/02/2022";
+        timeEl.innerHTML ="11:20";
+
+    }
     get displaycalc(){
 
         return this._displaycalc;
